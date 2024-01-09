1db091f7b4dd1ba0fea63c0703321fd61596fb8c
Alexandre Guerre <alexandre.guerre@sorbonne-universite.fr>
2024-01-09 10:33:51 +0100

    improves installation script

--- Software/binary_installation/install_APIv2.1/program_remote.sh
+++ Software/binary_installation/install_APIv2.1/program_remote.sh
@@ -2,6 +2,32 @@
 
 echo "using iceprog to program the remote"
 
+echo "FlashIsOK" > Flash.bin
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+check_return () {
+    if [[ "$1" = 0 ]]; then
+        echo -e " ${GREEN}-> OK${NC}"
+    else
+        echo -e " ${RED}-> NOK${NC}"
+        exit
+    fi
+}
+
 iceprog -o 0x0 bootloader/bootloader_pogobotv3/bootloader.bin
+check_return $?
+
 iceprog -o 0x240000 remocon/remocon_pogobotv3/gateware/remocon_pogobotv3.bin
+check_return $?
+
 iceprog -o 0x260000 remocon/remocon_pogobotv3/software/pogobios/pogobios.bin
+check_return $?
+
+iceprog -o 0x88000 Flash.bin
+check_return $?
+
+
+rm -f Flash.bin
